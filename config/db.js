66b235c307172e4e6c971e10767d84e1f9acc480
KevinAZM <159861313+KevinAZM@users.noreply.github.com>
2025-04-06 21:09:09 -0600

    db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 async function connectDB() {
   try {
-    await mongoose.connect('mongodb+srv://danielazuvaldo:Dazuva123@clusterdazuva.rqdbi.mongodb.net/productsCRUD');
+    await mongoose.connect('mongodb+srv://pablo:jrbehAcCfNLJBiOD@cluster0.je3g2jd.mongodb.net/productsCRUD');
     console.log('Conexión exitosa a MongoDB');
   } catch (error) {
     console.error('Error al conectar con MongoDB:', error);
